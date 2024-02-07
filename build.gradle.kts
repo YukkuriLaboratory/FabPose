09ec1e864090a8fb4452a70fce5fb421231a9f97
turtton <top.gear7509@turtton.net>
2024-02-07 17:19:00 +0900

    build: Change groovy scripts to kotlin

---
+++ build.gradle.kts
@@ -0,0 +1,89 @@
+plugins {
+    id("fabric-loom") version "1.4-SNAPSHOT"
+    id("maven-publish")
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
+}
+
+base {
+    archivesName.set(project.property("archives_base_name") as? String)
+    version = project.property("mod_version")!!
+    group = project.property("maven_group")!!
+}
+
+repositories {
+    // Add repositories to retrieve artifacts from in here.
+    // You should only use this when depending on other mods because
+    // Loom adds the essential maven repositories to download Minecraft and libraries from automatically.
+    // See https://docs.gradle.org/current/userguide/declaring_repositories.html
+    // for more information about repositories.
+
+    maven("https://oss.sonatype.org/content/repositories/snapshots")
+}
+
+dependencies {
+    // To change the versions see the gradle.properties file
+    minecraft("com.mojang:minecraft:${project.property("minecraft_version")}")
+    mappings("net.fabricmc:yarn:${project.property("yarn_mappings")}:v2")
+    modImplementation("net.fabricmc:fabric-loader:${project.property("loader_version")}")
+
+    // Fabric API. This is technically optional, but you probably want it anyway.
+    modImplementation("net.fabricmc.fabric-api:fabric-api:${project.property("fabric_version")}")
+    // Permissions API
+    modImplementation(include("me.lucko:fabric-permissions-api:0.1-SNAPSHOT")!!)
+
+    // Uncomment the following line to enable the deprecated Fabric API modules.
+    // These are included in the Fabric API production distribution and allow you to update your mod to the latest modules at a later more convenient time.
+
+    // modImplementation("net.fabricmc.fabric-api:fabric-api-deprecated:${project.property("fabric_version}")")
+}
+
+loom {
+    accessWidenerPath.set(file("src/main/resources/fabsit.accesswidener"))
+}
+
+tasks.processResources {
+    inputs.property("version", project.version)
+
+    filesMatching("fabric.mod.json") {
+        expand("version" to project.version)
+    }
+}
+
+tasks.withType<JavaCompile>().configureEach {
+    // Minecraft 1.18 (1.18-pre2) upwards uses Java 17.
+    options.release = 17
+}
+
+java {
+    // Loom will automatically attach sourcesJar to a RemapSourcesJar task and to the "build" task
+    // if it is present.
+    // If you remove this line, sources will not be generated.
+    withSourcesJar()
+}
+
+tasks.jar {
+    from("LICENSE") {
+        rename { "${it}_${project.base.archivesName}" }
+    }
+}
+
+// configure the maven publication
+publishing {
+    publications {
+        create<MavenPublication>("mavenJava") {
+            from(components.getByName("java"))
+        }
+    }
+
+    // See https://docs.gradle.org/current/userguide/publishing_maven.html for information on how to set up publishing.
+    repositories {
+        // Add repositories to publish to here.
+        // Notice: This block does NOT have the same function as the block in the top level.
+        // The repositories here will be used for publishing your artifact, not for
+        // retrieving dependencies.
+    }
+}
