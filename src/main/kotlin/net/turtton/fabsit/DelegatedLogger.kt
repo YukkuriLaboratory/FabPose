2c080698c8e2b5df04b19ec138af810d81833c47
turtton <top.gear7509@turtton.net>
2024-02-11 13:13:49 +0900

    feat: Add DelegatedLogger

---
+++ src/main/kotlin/net/turtton/fabsit/DelegatedLogger.kt
@@ -0,0 +1,15 @@
+package net.turtton.fabsit
+
+import kotlin.reflect.KProperty
+import net.fill1890.fabsit.FabSit
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+
+class DelegatedLogger {
+    private var logger: Logger? = null
+    operator fun getValue(thisRef: Any, property: KProperty<*>): Logger =
+        logger ?: run {
+            val className = thisRef::class.java.name.removeSuffix("\$Companion")
+            LoggerFactory.getLogger("${FabSit.MOD_ID}:$className").also { logger = it }
+        }
+}
