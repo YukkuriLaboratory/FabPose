ce7e780bd8fd18fa8a578f33c080b345d1bc15c9
turtton <top.gear7509@turtton.net>
2024-02-13 21:52:16 +0900

    test: Rework key press processes

--- src/clienttest/kotlin/test/keybind/TestPoseKeybindings.kt
+++ src/clienttest/kotlin/test/keybind/TestPoseKeybindings.kt
@@ -6,42 +6,55 @@ import kotlin.time.Duration.Companion.milliseconds
 import kotlin.time.Duration.Companion.seconds
 import kotlinx.coroutines.withContext
 import kotlinx.coroutines.withTimeout
+import net.fill1890.fabsit.entity.Pose
 import net.fill1890.fabsit.keybind.PoseKeybinds
 import net.minecraft.client.MinecraftClient
 import net.minecraft.client.option.KeyBinding
+import net.minecraft.client.util.InputUtil
+import net.yukulab.fabsit.extension.currentPose
 import net.yukulab.fabsit.extension.loop
 import runner.clientDispatcher
 
 object TestPoseKeybindings {
     suspend fun testSitKey(client: MinecraftClient) {
-        testKey(client, PoseKeybinds.sitKey)
+        testKey(client, PoseKeybinds.sitKey, Pose.SITTING)
     }
 
     suspend fun testLayKey(client: MinecraftClient) {
-        testKey(client, PoseKeybinds.layKey)
+        testKey(client, PoseKeybinds.layKey, Pose.LAYING)
     }
 
     suspend fun testSpinKey(client: MinecraftClient) {
-        testKey(client, PoseKeybinds.spinKey)
+        testKey(client, PoseKeybinds.spinKey, Pose.SPINNING)
     }
 
     suspend fun testSwimKey(client: MinecraftClient) {
-        testKey(client, PoseKeybinds.swimKey)
+        testKey(client, PoseKeybinds.swimKey, Pose.SWIMMING)
     }
 
-    private suspend fun testKey(client: MinecraftClient, key: KeyBinding) {
+    private suspend fun testKey(client: MinecraftClient, key: KeyBinding, pose: Pose) {
         val player = client.player.shouldNotBeNull()
         withContext(clientDispatcher) {
-            KeyBinding.setKeyPressed(key.defaultKey, true)
-            withClue("Checking player is sitting") {
-                withTimeout(1.seconds) {
+            val inputKey = InputUtil.Type.KEYSYM.createFromCode(InputUtil.GLFW_KEY_H)
+            key.setBoundKey(inputKey)
+            KeyBinding.updateKeysByCode()
+            withClue("Checking player is posing $pose") {
+                withTimeout(2.seconds) {
                     loop(50.milliseconds) {
-                        val vehicle = player.vehicle ?: return@loop true
-                        !vehicle.isAlive
+                        KeyBinding.onKeyPressed(inputKey)
+                        val validPlayerState =
+                            player.currentPose == pose && (pose == Pose.SWIMMING || player.vehicle != null)
+                        val validPlayerVisible = if (pose in setOf(Pose.LAYING, Pose.SPINNING)) {
+                            player.isInvisible
+                        } else {
+                            !player.isInvisible
+                        }
+                        !validPlayerState || !validPlayerVisible
                     }
                 }
             }
-            KeyBinding.setKeyPressed(key.defaultKey, false)
+            key.setBoundKey(key.defaultKey)
+            KeyBinding.updateKeysByCode()
         }
     }
 }
